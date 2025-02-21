a480e21dfd257815069a35cd0530f2381a7e1ab8
jnickolin <jojo@nickolindigital.com>
2025-02-20 17:34:59 -0800

    Added playlist nav styles.

--- src/PlaylistNav/PlaylistNav.js
+++ src/PlaylistNav/PlaylistNav.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import styles from './PlaylistNav.module.css'
+
 
 function PlaylistNav() {
     const playlists = ['tswift tuesdays', '80R08', 'golden age disney channel', 'cause of death', 'another playlist', 'tell me ur a millennial'];
     const navLinks = playlists.map(playlist => {
         return (
-            <a href={'/' + playlist}>
+            <a href={'/' + playlist} className={styles.navitem}>
            &nbsp;{playlist}
         </a>
         )
