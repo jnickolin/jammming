98490fd7c317242cdd28a2dd06fb68de0d70e8af
jnickolin <jojo@nickolindigital.com>
2025-03-04 09:44:22 -0800

    Add track module and track data object

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Track.module.css';
+import trackData from './TrackData.js';
 
 function Track() {
     return (
         <div className={styles.trackContainer}>
-            <img src='./album-art-temp.png' alt='album art'/>
-            <h2>Bet On It</h2>
-            <p>Troy</p>
+            <img src={trackData.src} alt='album art'/>
+            <h2>{trackData.title}</h2>
+            <p>{trackData.artist}</p>
         </div>
     );
 }
--- src/Track/TrackData.js
+++ src/Track/TrackData.js
@@ -2,32 +2,14 @@ import React from 'react';
 
 const trackData = [
     {
-        title: 'This is Me',
-        src: '',
-        album: 'Camp Rock',
-        artist: 'Demi Lovato & Joe Jonas',
-        bpm: 125
-        length: 3.07
-
-    },
-    {
         title: 'What Dreams Are Made Of',
         src: '',
         album: 'The Lizzie Maguire Movie',
         artist: 'Hillary Duff',
-        bpm: 95
+        bpm: 95,
         length: 4.72
 
-    },
-    {
-        title: 'Best of Both Worlds',
-        src: '',
-        album: 'Hannah Montana',
-        artist: 'Hannah Montana',
-        bpm: 105
-        length: 3.45
-
-    }    
+    } 
 
 ]
 
